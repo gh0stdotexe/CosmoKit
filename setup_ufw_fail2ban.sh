7a176ea6d6f09d1590fcf7fbb36e58f4ed8f7e35
gh0st | WhisperNode <49853598+gh0stdotexe@users.noreply.github.com>
2022-03-16 22:28:42 -0400

    Create setup_ufw_fail2ban.sh
    Add script for installing UFW, configuring tendermint ports, + installing fail2ban

---
+++ setup_ufw_fail2ban.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# install UFW & port rules
+
+apt-get install ufw -y
+
+ufw default allow outgoing
+ufw default deny incoming
+ufw allow ssh/tcp
+ufw limit ssh/tcp
+ufw allow 26656
+ufw allow 26657
+ufw allow 9093
+ufw allow 16524/tcp
+ufw limit 16524/tcp
+ufw deny 22/tcp
+ufw allow 1317
+ufw enable
+
+# install fail2ban
+
+apt-get install fail2ban -y
+
+cp /etc/fail2ban/jail.{conf,local}
